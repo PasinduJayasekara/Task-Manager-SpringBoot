c812d43952ceb6b8c8701df698579b7784ffc257
PasinduJayasekara <jmprj.003@gmail.com>
2026-05-15 21:29:36 +0530

    add docker setup with choreo config

--- backend/src/main/java/com/pasindu/backend/config/CorsConfig.java
+++ backend/src/main/java/com/pasindu/backend/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:3000")
         );
 
         configuration.setAllowedMethods(
